e43574a2acb8f4df0430d6e37fc5576a2942860b
Jordan Sissel <jls@semicomplete.com>
2016-10-09 06:33:00 +0000

    Press the button game.

--- buttons.py
+++ buttons.py
@@ -1,15 +1,16 @@
 import RPi.GPIO as GPIO
 import time
+import random
+from neopixel import *
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-class Circuit(object):
+class Circuit:
 	open = 1
-	closed = 1
+	closed = 0
 
 class GPIOSwitch:
-	DEFAULT_DEBOUNCE_PERIOD = 0.010
+	DEFAULT_DEBOUNCE_PERIOD = 0.030
 
 	def __init__(self, pin, debounce_period=DEFAULT_DEBOUNCE_PERIOD):
 		self.pin = pin
@@ -19,11 +20,11 @@ class GPIOSwitch:
 		self.__previous_value = None
 		self.__previous_time = None
 		self.__ignore_until = 0
-		self.check()
+		self.check(time.time())
 
-	def check(self):
+	def check(self, timestamp):
 		value = GPIO.input(self.pin)
-		return self.update(value, time.time())
+		return self.update(value, timestamp)
 
 	def update(self, value, timestamp):
 		# Ignore updates during debounce period
@@ -40,16 +41,73 @@ class GPIOSwitch:
 		self.__previous_value = value
 		return value
 
-switches = [
-	GPIOSwitch(26),
-	GPIOSwitch(19),
-	GPIOSwitch(13),
-	GPIOSwitch(6),
-	GPIOSwitch(5)
-]
+class Game:
+	LED_OFF = Color(0,0,0)
 
-while True:
-	for i, switch in enumerate(switches):
-		state = switch.check()
-		if state != None:
-			print "Button %d: %d" % (i, state)
+	def __init__(self):
+		self.switches = [
+			GPIOSwitch(5),
+			GPIOSwitch(6),
+			GPIOSwitch(13),
+			GPIOSwitch(19),
+			GPIOSwitch(26)
+		]
+
+		self.lights = Adafruit_NeoPixel(5, 18, 800000, 5, False, 50, 0, ws.WS2811_STRIP_GRB)
+		self.startup()
+
+	def startup(self):
+		self.lights.begin()
+
+		# Startup light sequence.
+		for x in range(5):
+			self.lights.setPixelColor(4-x, Color(255,0,255))
+			self.lights.show()
+			time.sleep(0.1)
+			self.lights.setPixelColor(4-x, Game.LED_OFF)
+			self.lights.show()
+
+		for x in range(5):
+			self.lights.setPixelColor(x, Color(255,0,255))
+			self.lights.show()
+			time.sleep(0.1)
+			self.lights.setPixelColor(x, Game.LED_OFF)
+			self.lights.show()
+
+		self.__target = None
+		self.target(random.randint(0,4))
+
+	def target(self, button):
+		self.clear()
+		self.__target = button
+		self.lights.setPixelColor(4-button, Color(0, 255, 0))
+		self.lights.show()
+
+	def clear(self):
+		for x in range(5):
+			self.lights.setPixelColor(x, Game.LED_OFF)
+		self.lights.show()
+
+	def pressed(self, button):
+		if button != self.__target:
+			self.lights.setPixelColor(4-button, Color(255, 0, 0))
+			self.lights.show()
+		else:
+			self.target(random.randint(0,4))
+
+	def loop(self):
+		while True:
+			self.tick();
+
+	def tick(self):
+		t = time.time()
+		states = [switch.check(t) for switch in self.switches]
+		for i, state in enumerate(states):
+			if state == Circuit.open:
+				print "%f: Button %d: release" % (t, i)
+			elif state == Circuit.closed:
+				print "%f: Button %d: pressed" % (t, i)
+				self.pressed(i)
+
+
+Game().loop()
