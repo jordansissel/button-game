13f175d4b7c96123ad554b320cb394061a6f0ee1
Jordan Sissel <jls@semicomplete.com>
2016-10-18 09:52:01 -0700

    Rewrite from unittest to pytest

--- tests/gpio_switch_test.py
+++ tests/gpio_switch_test.py
@@ -1,27 +1,29 @@
-from mock import Mock
+import mock
 from gpio_switch import GPIOSwitch
 from circuit import Circuit
+import pytest
 
 O = Circuit.open
 C = Circuit.closed
 
-class TestGPIOSwitch:
-	@classmethod
-	def setup_class(self):
-		self.mock_gpio = Mock()
+@pytest.fixture
+def gpio():
+	gpio = mock.Mock()
+	gpio.setup = mock.Mock()
+	return gpio
 
-	def assertStates(self, input, output, **kwargs):
-		self.mock_gpio.input = Mock(side_effect = input)
-		switch = GPIOSwitch(0, self.mock_gpio, **kwargs)
+def assertStates(gpio, input, output, **kwargs):
+	gpio.input = mock.Mock(side_effect=input)
+	switch = GPIOSwitch(0, gpio, **kwargs)
 
-		# initial state is read during __init__ of GPIOSwitch
-		assert switch.peek() == input[0]
+	# initial state is read during __init__ of GPIOSwitch
+	assert switch.peek() == input[0]
 
-		results = [switch.check(i) for i in range(len(input) - 1)]
-		assert results == output
+	results = [switch.check(i) for i in range(len(input) - 1)]
+	assert results == output
 
-	def testCheckReturnsCircuitChanges(self):
-		self.assertStates([O, O, O, C, C, O], [None, None, C, None, O], debounce_period=0)
+def testCheckReturnsCircuitChanges(gpio):
+	assertStates(gpio, [O, O, O, C, C, O], [None, None, C, None, O], debounce_period=0)
 
-	def testCheckDebouncing(self):
-		self.assertStates([O, C, O, C, O], [C, None, None, O], debounce_period=2)
+def testCheckDebouncing(gpio):
+	assertStates(gpio, [O, C, O, C, O], [C, None, None, O], debounce_period=2)
